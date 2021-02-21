c1d0a3ea017fe82fa80ef1c8a15c7a307fbd8e43
colin1031 <59859917+colin1031@users.noreply.github.com>
2021-02-21 19:32:46 +0800

    Update x_dm_mining_twitter_script.py

--- x_dm_mining_twitter_script.py
+++ x_dm_mining_twitter_script.py
@@ -8,16 +8,25 @@ Created on Sat Feb 20 18:12:06 2021
 """
 if need:
 pip install tweepy
-pip install twint
+pip uninstall twint -y
+pip install --user --upgrade git+https://github.com/twintproject/twint.git@origin/master#egg=twint
+pip install asyncio
+pip install nest_asyncio
+
 """
 
 """
 import
 """
 import os
+import datetime
 import tweepy
 import time
 import twint
+import pandas as pd
+import asyncio
+import nest_asyncio
+
 """
 Setting directory
 """
@@ -31,15 +40,50 @@ os.chdir(mac_path)
 
 os.getcwd()
 
+
+"""
+Mining Old tweets through Twint
+"""
+
+#configuration
+config = twint.Config()
+config.Search = ['Xrp' or "Ripple"]
+config.Lang = "en"
+config.Limit = 100000000000000000000
+config.Since = '2020-02-01'
+config.Until = '2021-02-01'
+
+#If we want to store in json
+# config.Store_json = True
+# config.Output = "custom_out.json"
+
+#If we want to store in csv
+config.Store_csv = True
+config.Output = "xrp_or_ripple_twint.csv"
+
+#Check problem: This event loop is already running exist
+loop = asyncio.get_event_loop()
+loop.is_running()
+
+#If True, run this command
+if loop.is_running() == True:
+    nest_asyncio.apply()
+
+#running search
+twint.run.Search(config)
+
+
+"""
+If we want to stream tweets through tweepy
+"""
+#Keys entries
 access_token=''
 access_token_secret=''
 
 consumer_key=''
 consumer_secret=''
 
-"""
-Verifying Credentials & test authentication
-"""
+#Verifying Credentials & test authentication
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
@@ -51,11 +95,7 @@ try:
 except:
     print("Error during authentication")
 
-
-"""
-Mining Tweets (Streaming)
-"""
-
+#Mining Tweets (Streaming)
 class listener(tweepy.StreamListener):
     def on_status(self, status):
         with open('data-streaming-tweets.txt', 'a',encoding="utf-8") as f:
@@ -92,42 +132,15 @@ mystream.filter(track=['XRP','Xrp','xrp','RIPPLE','Ripple','ripple'],is_async=Tr
 
 #is_async=True #background streaming ,will not run in console
     
-"""
-if is_async=False, control c to stop    
-"""
-    
-"""
-disconnecting background stream (if is_async=True) 
-"""
+#if we set is_async=False, control c to stop    
+
+#disconnecting background stream (if is_async=True) 
 mystream.disconnect()
 
-"""
-Close file after mining
-"""
+#Close file after mining
 f.close()
 
-"""
-Read txt file
-"""
+#Read txt file
 f = open(mac_path+os.sep+'data-streaming-tweets.txt', "r",encoding="utf-8")
 f.read()
 f.close()
-
-"""
-Mining Old tweets
-"""
-
-#configuration
-config = twint.Config()
-config.Search = "bitcoin"
-config.Lang = "en"
-config.Limit = 100
-config.Since = "2019–04–29"
-config.Until = "2019–09–29"
-config.Store_json = True
-config.Output = "custom_out.json"
-
-#running search
-twint.run.Search(config)
-
-
