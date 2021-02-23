511f06dbd86aa43d14a26c833d0ae02bd8de64d9
colin1031 <59859917+colin1031@users.noreply.github.com>
2021-02-23 17:28:12 +0800

    Update x_dm_mining_twitter_script.py

--- x_dm_mining_twitter_script.py
+++ x_dm_mining_twitter_script.py
@@ -49,24 +49,52 @@ config = twint.Config()
 config.Search = ['Xrp' or "Ripple"] #if tweet include xrp or ripple (Capital letter or not doesn't matter)
 config.Lang = "en"
 config.Limit = 100000000000000000000
-config.Since = '2020-02-01'
-config.Until = '2021-02-01'
 
-#If we want to store in json
-# config.Store_json = True
-# config.Output = "custom_out.json"
+start_date = '2020-10-01'
+timeperiod = 365
 
-#If we want to store in csv
-config.Store_csv = True
-config.Output = "xrp_or_ripple_twint.csv"
+for i in range(timeperiod):
+    
+    since = (start_timetime + datetime.timedelta(days = i)).strftime('%Y-%m-%d')
+    until = (start_timetime + datetime.timedelta(days = i+1)).strftime('%Y-%m-%d')
+    
+    config.Since = since
+    config.Until = until
 
-#Check problem: This event loop is already running exist
-loop = asyncio.get_event_loop()
-loop.is_running()
+    #If we want to store in json
+    # config.Store_json = True
+    # config.Output = "custom_out.json"
 
-#If True, run this command
-if loop.is_running() == True:
-    nest_asyncio.apply()
+    #If we want to store in csv
+    config.Store_csv = True
+    config.Output = "raw_xrp_or_ripple_twint_{}.csv".format(since)
 
-#running search
-twint.run.Search(config)
+    #Check problem: This event loop is already running exist
+    loop = asyncio.get_event_loop()
+    loop.is_running()
+
+    #If True, run this command
+    if loop.is_running() == True:
+        nest_asyncio.apply()
+
+    #running search
+    twint.run.Search(config)
+
+"""
+Read raw data (tweets through Twint with specific time range)
+"""
+raw_data_tweets_thro_twint=pd.read_csv(win_path_data+os.sep+'raw_xrp_or_ripple_twint_1.csv')
+
+"""
+Raw Data Cleaning
+"""
+raw_data_tweets_thro_twint.columns
+extract_columns_list_cleaning_data_use=['created_at','date','time','username','tweet','language',\
+                                        'mentions','replies_count','retweets_count','likes_count','hashtags','cashtags','retweet'\
+                                            ]
+cleaned_data_tweets_thro_twint=raw_data_tweets_thro_twint[extract_columns_list_cleaning_data_use]
+cleaned_data_tweets_thro_twint.columns
+
+"""
+Extract number of followers from each user
+"""
