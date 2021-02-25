d5ccbad53a23a96b58460861c7305fe31e5a77d2
colin1031 <59859917+colin1031@users.noreply.github.com>
2021-02-25 20:53:27 +0800

    Update raw_data_cleaning.py

--- raw_data_cleaning.py
+++ raw_data_cleaning.py
@@ -9,8 +9,7 @@ Created on Thu Feb 25 20:51:46 2021
 """
 Raw Data Cleaning
 """
-# path_raw_data='/Users/colinko/Documents/Colin/HKU/7036/X-dm/raw_data'
-path_raw_data=r'D:\iCloudDrive\Documents\Colin\HKU\7036\X-dm\raw_data'
+path_raw_data=''
 
 raw_data_tweets=pd.read_csv(path_raw_data+os.sep+'raw_data_tweets.csv')
 
@@ -30,5 +29,5 @@ cleaning_data_tweets_1.to_csv(cleaning_data_path+os.sep+'cleaning_data_tweets_1.
 
 cleaning_data_tweets_1=pd.read_csv(cleaning_data_path+os.sep+'cleaning_data_tweets_1.csv')
 
-cleaning_data_path=r'D:\iCloudDrive\Documents\Colin\HKU\7036\X-dm\cleaning_data'
+cleaning_data_path=''
 cleaning_data_tweets_1.to_csv(cleaning_data_path+os.sep+'cleaning_data_tweets_1.csv',index=False)
