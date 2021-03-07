cccc224f734233a5732a0fbb8a229ee1d0e8c96d
Zongyu-Lyu <78085629+Zongyu-Lyu@users.noreply.github.com>
2021-03-07 21:05:33 +0800

    Update Ripple Financial Data.py

--- Ripple Financial Data/Ripple Financial Data.py
+++ Ripple Financial Data/Ripple Financial Data.py
@@ -26,26 +26,31 @@ ripple = pd.read_csv(r'C:\Users\Zongyu Lyu\Desktop\Raw Ripple Price.csv')
 # daily return
 daily_return = ripple['Adj Close'].pct_change(1)
 
-# volatility for every 30 days windows
+# Volatility for every 30 days windows
 values = daily_return.values
 volatility = []
 for day in range(31,len(values)):
     if day > len(values) - 1:
         break
     volatility.append(np.std(values[day-30:day], ddof=1))
+    
+# Fill the empty rows of volatility so that it has the same amount of rows as Raw Ripple Price.csv
 match_row = list(range(31))+volatility
 volatility = pd.DataFrame({'30 days volatility':match_row})
-
-# Price,return and volatility plotting
-ripple['Adj Close'].plot(grid=True, figsize=(8,5))
-daily_return.plot(grid=True, figsize=(8,5))
-volatility.plot(grid=True, figsize=(8,5))
 
 # Data cleanning
 data_cleaning = pd.DataFrame({'Date':ripple['Date'],
                               'Adj Close':ripple['Adj Close'],
                               'daily return':daily_return})
 cleaned = pd.concat([data_cleaning,volatility],axis=1)
+
+# Select the row from 2020-03-01 to 2021-03-05
+cleaned_data = cleaned[61:428]
+
+# Price,return and volatility plotting
+cleaned_data['Adj Close'].plot(grid=True, figsize=(8,5))
+cleaned_data['daily return'].plot(grid=True, figsize=(8,5))
+cleaned_data['30 days volatility'].plot(grid=True, figsize=(8,5))
 cleaned.to_csv(r'C:\Users\Zongyu Lyu\Desktop\Cleaned Ripple Financial Data.csv',index=False)
 
 
